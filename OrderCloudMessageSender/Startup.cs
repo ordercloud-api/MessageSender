f2700a3665de72f4ca763aaf21e1f85b779434d9
Jeff Ilse <jli@ilseus.net>
2017-07-16 21:37:38 -0400

    verify sender by x-oc-hash header value

--- OrderCloudMessageSender/Startup.cs
+++ OrderCloudMessageSender/Startup.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -36,8 +41,30 @@ namespace OrderCloudMessageSender
         {
 	        loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+			
+	        app.Use(async (context, next) =>
+	        {
+				string body = await new StreamReader(context.Request.Body).ReadToEndAsync();
+		        var keyBytes = Encoding.UTF8.GetBytes(Configuration["OCHashKey"]);
+		        var dataBytes = Encoding.UTF8.GetBytes(body);
+		        var hmac = new HMACSHA256(keyBytes);
+		        var hmacBytes = hmac.ComputeHash(dataBytes);
+		        var hash = Convert.ToBase64String(hmacBytes);
+
+		        if (context.Request.Headers["X-oc-hash"] != hash)
+		        {
+			        context.Response.StatusCode = 401;
+			        await context.Response.WriteAsync("the sender is not validated. OCHashKey in appsettings.json must match the shared key in the OrderCloud Message sender config.");
+			        //throw new Exception("The signature does not match. The sender is not verified.");
+		        }
+		        else
+		        {
+			        context.Request.Body = new MemoryStream(dataBytes);
+			        await next();
+				}
+	        });
 
             app.UseMvc();
         }
-    }
+	}
 }
